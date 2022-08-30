e487461394a3c7881f36fa0b89f7830a33963ca5
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-30 12:18:06 +0300

    adding an evant listener to the form

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 const FoxAPI = "https://randomfox.ca/floof"
-const h2 = document.createElement("h2");
-h2.textContent = "javascript content";
-document.querySelector("body").appendChild(h2);
-
+const h6 = document.createElement("h6");
+h6.textContent = "copyrightsreserved@mildred.koskei";
+document.querySelector("body").appendChild(h6);
 
 fetch(FoxAPI)
 .then(resp => resp.json())
 .then(data=> console.log(data))
+
+const addLikes = document.querySelector('#like-button')
+
+
+const form = document.querySelector('#comment-form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    showComment(e.target.comment.value)
+})
+function showComment(comment) {
+    console.log(comment);
+    const li = document.createElement("li");
+    let btn = document.createElement('button')
+    btn.addEventListener('click', deleteHandle)
+    btn.textContent = 'delete'
+    li.textContent = comment.content;
+    li.appendChild(btn)
+    commentsList.append(li);
+  }
+  
+  function deleteHandle(e){
+      e.target.parentNode.remove()
+  }
+  //function displayLikes() {
+    //likeCount.textContent = `${likes} likes`;
+  //}
+
