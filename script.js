9a6448fbc0f863378da73d5d734f187d30640211
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-30 11:12:55 +0300

    add API url and body structure

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const FoxAPI = "https://randomfox.ca/floof"
 const h2 = document.createElement("h2");
 h2.textContent = "javascript content";
 document.querySelector("body").appendChild(h2);
+
+
+fetch(FoxAPI)
+.then(resp => resp.json())
+.then(data=> console.log(data))
