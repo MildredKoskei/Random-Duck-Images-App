4664bfe0776f767cdac7f982b478eda55e6a84fa
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-30 13:48:13 +0300

    adding a function that displays fox images

--- script.js
+++ script.js
@@ -1,38 +1,80 @@
-const FoxAPI = "https://randomfox.ca/floof"
-const h6 = document.createElement("h6");
-h6.textContent = "copyrightsreserved@mildred.koskei";
-document.querySelector("body").appendChild(h6);
-let likes;git 
+document.addEventListener("DOMContentLoaded", ()=>{
+    fetch("https://randomfox.ca/floof")
+    .then(resp => resp.json())
+    .then(data =>displayFoxImage(data))
+       
+    
 
-fetch(FoxAPI)
-.then(resp => resp.json())
-.then(data=> console.log(data))
+function displayFoxImage(data){
+    console.log(data)
+    let cardImage = document.querySelector('#card-image')
+    cardImage.src = data.image
 
-const likesbutton = document.querySelector('#like-button').addEventListener('click', ()=>{
-    likes += 1;
-    displayLikes();
-});
-
-
-const form = document.querySelector('#comment-form').addEventListener('submit', (e)=>{
-    e.preventDefault()
-    showComment(e.target.comment.value)
+}
 })
-function showComment(comment) {
-    console.log(comment);
-    const li = document.createElement("li");
-    let btn = document.createElement('button')
-    btn.addEventListener('click', deleteHandle)
-    btn.textContent = 'delete'
-    li.textContent = comment.content;
-    li.appendChild(btn)
-    commentsList.append(li);
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const FoxAPI = "https://randomfox.ca/floof"
+// const h6 = document.createElement("h6");
+// h6.textContent = "copyrightsreserved@mildred.koskei";
+// document.querySelector("body").appendChild(h6);
+//  const cardImage = document.querySelector('#card-image')
+// const likeCount = document.querySelector("#like-count");
+// let likes;
+
+// fetch(FoxAPI)
+// .then(resp => resp.json())
+// .then(data => displayFoxImage(data));
+
+// function displayFoxImage(data){
+//     data.forEach(showFoxImage)
+// }
+// function showFoxImage(fox){
+//     let foxImage = document.createElement('img')
+//     foxImage.src = FoxAPI;
+//     cardImage.appendChild(foxImage)
+
+
+// }
+
+// const likesbutton = document.querySelector('#like-button').addEventListener('click', ()=>{
+//     likes += 1;
+//     addLikes()
+// })
+
+
+// const form = document.querySelector('#comment-form').addEventListener('submit', (e)=>{
+//     e.preventDefault()
+//     showComment(e.target.comment.value)
+//     e.reset()
+// })
+// function showComment(comment) {
+//     console.log(comment);
+//     const li = document.createElement("li");
+//     let btn = document.createElement('button')
+//     btn.addEventListener('click', deleteHandle)
+//     btn.textContent = 'delete'
+//     li.textContent = comment.content;
+//     li.appendChild(btn)
+//     commentsList.append(li);
+//   }
   
-  function deleteHandle(e){
-      e.target.parentNode.remove()
-  }
-  function displayLikes() {
-    likeCount.textContent = `${likes} likes`;
-  }
+//   function deleteHandle(e){
+//       e.target.parentNode.remove()
+//   }
+//   function addLikes() {
+//     likeCount.textContent = `${likes} likes`;
+//   }
 
